9e477c6d7329ba4503aeb433531f87fb2b5ef4cd
varunpats <varunpatil2502@gmail.com>
2023-07-16 22:25:17 +0530

    Initial commit

---
+++ src/components/Child.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Child = () => {
+    return (
+        <>
+            <h2>Child Component</h2>
+            <button>Show Modal</button>
+        </>
+    )
+}
+
+export default Child;
---
+++ src/components/Parent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Parent = () => {
+    <h2>Parent Component</h2>
+}
+
+export default Parent;
